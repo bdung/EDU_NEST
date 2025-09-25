8c0c7edbcebb21de7887f90e9283824bdeb90bd1
bdung0855 <bdung0855@gmail.com>
2025-09-25 10:02:36 +0700

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
           path="/exam" 
           element={
             <ProtectedRoute allowedRoles={["admin","user"]}>
-              <ExamView />
+              <SurveyComponent />
             </ProtectedRoute>
           } 
         />
